b7be83b34298a192242bc0c9fe920be2379f10e9
Tanisha Howell <57296464+tanishahowell@users.noreply.github.com>
2020-03-30 00:25:26 -0400

    Updated Profile page and put style.css file in styles folder

--- public/js/stocks.js
+++ public/js/stocks.js
@@ -1,21 +1,21 @@
 $(document).on('click'), 'stocks-search-button', function() {
 
-    $('.stock-search-info').empty();
+    $('#stock-details').empty();
   
 var type = $(this).data('type');
 
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-summary?region=US&lang=en",
+	"url": "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-financials?symbol=AMRN",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-        "x-rapidapi-key": "118d7e6541mshbc2cbcaf840195ep13f35fjsnd7ca4eaaff11"
-        
-        $('.stock-search-info').append(searchDiv);
+		"x-rapidapi-key": "118d7e6541mshbc2cbcaf840195ep13f35fjsnd7ca4eaaff11"
+
+        $('#stock-details').append(searchDiv);
 	}
-}
+
 
 $.ajax(settings).done(function (response) {
 	console.log(response);
